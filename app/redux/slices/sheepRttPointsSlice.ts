a34e0442b947ad297ca5ef375a8336553db7311a
Toni Vucic <tonivuc@gmail.com>
2022-06-17 16:59:46 +0200

    Basic implementation with functionality verified

--- app/redux/slices/sheepRttPointsSlice.ts
+++ app/redux/slices/sheepRttPointsSlice.ts
@@ -2,6 +2,8 @@ import {createSlice, PayloadAction} from '@reduxjs/toolkit';
 import {RootState} from '../store';
 import {Feature, FeatureCollection, Point} from "geojson";
 
+const USE_ONLY_EVERY_10TH_MEASUREMENT = false;
+
 const sheepRttPoints = createSlice({
     name: 'sheepRttPoints',
     initialState: {value: <FeatureCollection<Point>> {type: "FeatureCollection", features: []}},
@@ -20,13 +22,62 @@ const sheepRttPoints = createSlice({
             }
         },
         setSheepRttPoints: (state, action: PayloadAction<FeatureCollection<Point>>) => {
-            state.value = action.payload;
+            if (USE_ONLY_EVERY_10TH_MEASUREMENT) {
+                const allRTTPoints = action.payload
+                const every10thRTTMeasurement = onlyEver10thMeasurement(allRTTPoints)
+            }
+            else {
+                console.log(onlyEver10thMeasurement(action.payload));
+                state.value = action.payload;
+            }
+            
         },
         removeSheepRttPoints: (state) => {
             state.value = {type: "FeatureCollection", features: []};
         },
     },
 });
+
+function onlyEver10thMeasurement(allRTTPoints : FeatureCollection<Point>) {
+    //const filteredPoints:  FeatureCollection<Point> = null;
+    const rttPointsSplitIntoArrays = splitIntoArrayForEachTagID(allRTTPoints);
+    console.log(rttPointsSplitIntoArrays)
+    return getEvery10thMeasurement(rttPointsSplitIntoArrays);
+}
+
+function splitIntoArrayForEachTagID(allRTTPoints : FeatureCollection<Point>): Map<number, Feature<Point>[]> {
+    const filteredPoints:  Feature<Point>[] = allRTTPoints.features;
+    const rttPointsSortedBySheepId = new Map<number, Feature<Point>[]>();
+    filteredPoints.forEach((point) => {
+        if (rttPointsSortedBySheepId.has(point?.properties?.tid)) {
+            rttPointsSortedBySheepId.get(point?.properties?.tid)!.push(point)
+        }
+        else {
+            rttPointsSortedBySheepId.set(point?.properties?.tid,[point])
+        }
+    })
+    return rttPointsSortedBySheepId;
+}
+
+function getEvery10thMeasurement(rttPointsInArrays : Map<number, Feature<Point>[]>) {
+    const newArray: Feature<Point>[] = [];
+    rttPointsInArrays.forEach((keyValue) => {
+        let counter = 0;
+        keyValue.forEach((feature, tagId) => {
+            if (counter === 0) {
+                newArray.push(feature);
+                counter++;
+            }
+            else if (counter === 9) {
+                counter = 0;
+            }
+            else {
+                counter++;
+            }
+        })
+    })
+    return newArray;
+}
 
 export const {storeSheepRttPoint, setSheepRttPoints, removeSheepRttPoints} = sheepRttPoints.actions;
 
